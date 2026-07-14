12eb4ec2dc7404b4995fef10955230c9301167a3
Muhammad Uzair <muhduzair0179@gmail.com>
2026-07-14 18:44:39 +0500

    Implement A* pathfinding and robot navigation

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from grid_map import create_grid
+from astar import astar
+from robot import Robot
 
+grid = create_grid()
+
+start = (0,0)
+goal = (19,19)
+
+path = astar(grid,start,goal)
+
+robot = Robot(grid,path)
+
+robot.navigate()
