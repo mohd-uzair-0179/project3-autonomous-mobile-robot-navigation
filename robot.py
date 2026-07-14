b9b5ece61b2aa75257c2e41b22355d97c4b2224c
Muhammad Uzair <muhduzair0179@gmail.com>
2026-07-14 18:45:34 +0500

    Implement robot navigation with LIDAR simulation

--- robot.py
+++ robot.py
@@ -1,1 +1,39 @@
+import matplotlib.pyplot as plt
 
+from lidar import simulate_lidar
+
+class Robot:
+
+    def __init__(self,grid,path):
+
+        self.grid=grid
+
+        self.path=path
+
+    def navigate(self):
+
+        plt.imshow(self.grid,cmap="gray_r")
+
+        x=[]
+
+        y=[]
+
+        for p in self.path:
+
+            simulate_lidar(p)
+
+            x.append(p[1])
+
+            y.append(p[0])
+
+        plt.plot(x,y,'r',linewidth=2)
+
+        plt.scatter(x[0],y[0],c='green',label='Start')
+
+        plt.scatter(x[-1],y[-1],c='blue',label='Goal')
+
+        plt.legend()
+
+        plt.title("Autonomous Robot Navigation")
+
+        plt.show()
